56766f485df592f346a74cedd5150af5ab8db40d
pulsecodify <pulsecodi@gmail.com>
2023-11-05 13:20:41 -0500

    Ajuste en las constantes

--- app/configs/constants.php
+++ app/configs/constants.php
@@ -1,11 +1,11 @@
 <?php
 
 // Definición de una constante llamada 'BASEPATH' que almacena la ruta al directorio actual donde se encuentra este archivo.
-define('BASEPATH', dirname(__FILE));
+define('BASEPATH', dirname(__FILE__));
 
 // Definición de constantes para rutas de la aplicación
 define('DS', DIRECTORY_SEPARATOR);  // 'DS' se define como el separador de directorios de la plataforma actual.
-define('PROJECT_PATH', dirname(dirname(__DIR)) . DS);  // 'PROJECT_PATH' almacena la ruta al directorio raíz del proyecto.
+define('PROJECT_PATH', dirname(dirname(__DIR__)) . DS);  // 'PROJECT_PATH' almacena la ruta al directorio raíz del proyecto.
 
 // Directorios en la carpeta 'app'
 define('APP_PATH', PROJECT_PATH . 'app' . DS);
