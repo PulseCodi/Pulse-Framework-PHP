d571ec2b349fcbeb91cebf30a47a1ada7480aee7
pulsecodify <pulsecodi@gmail.com>
2023-11-05 22:51:46 -0500

    Reajuste

--- app/libraries/Csrf.php
+++ app/libraries/Csrf.php
@@ -1,61 +1,91 @@
-<?php
+  <?php
+  class Csrf
+  {
+      private $tokenName;
+      private $tokenLength;
+      private $tokenExpiration;
+      public function __construct()
+      {
+          // Load CSRF configuration
+          require CONFIGS_PATH . 'config.php';
 
-class Csrf
-{
-    private $tokenName;
-    private $tokenLength;
-    private $tokenExpiration;
+          // Ensure all values are set in the config file 
+          if (!isset($config['csrf_token_name'], $config['csrf_token_length'], 
+              $config['csrf_token_expiration_time'])) {
+              throw new ErrorHandler("CSRF configuration is not set correctly");
+          }
+          $this->setTokenData($config['csrf_token_name'], 
+          $config['csrf_token_length'], 
+          $config['csrf_token_expiration_time']);
+      }
+      private function setTokenData($name, $length, $expiration)
+      {
+          $this->tokenName = $name;
+          $this->tokenLength = $length;
+          $this->tokenExpiration = $expiration;
+      }
+      public function generateToken($formIdentifier)
+      {
+          if (!is_string($formIdentifier)) {
+              throw new ErrorHandler("Form Identifier must be a string");
+          }
+          if ($this->_isTokenInvalidOrExpired($formIdentifier)) {
+              $this->_setTokenAndExpiration($formIdentifier);
+          }
+          return $_SESSION[$this->tokenName][$formIdentifier];
+      }
+      public function verifyToken($requestData, $formIdentifier)
+      {
+          if (!is_array($requestData) || !is_string($formIdentifier)) {
+              throw new ErrorHandler("Invalid input parameters");
+          }
+          $this->_tokenExists($requestData, $formIdentifier);
+          return $this->isTokenValidAndNotExpired($requestData, $sessionData, $formIdentifier);
+      }
 
-    public function __construct()
-    {
-        require CONFIGS_PATH . 'config.php';
-        // Accede a la configuración de token CSRF desde tu archivo de configuración
-        $this->tokenName = $config['csrf_token_name'];
-        $this->tokenLength = $config['csrf_token_length'];
-        $this->tokenExpiration = $config['csrf_token_expiration_time'];
-    }
-
-    public function generateToken($formIdentifier)
-    {
-        if (!isset($_SESSION[$this->tokenName][$formIdentifier]) || time() >= $_SESSION[$this->tokenName . '_expiration'][$formIdentifier]) {
-            $_SESSION[$this->tokenName][$formIdentifier] = $this->_generateRandomToken();
-            $_SESSION[$this->tokenName . '_expiration'][$formIdentifier] = time() + $this->tokenExpiration;
-        }
-
-        return $_SESSION[$this->tokenName][$formIdentifier];
-    }
-
-    public function verifyToken($requestData, $formIdentifier)
-    {
-        $this->_ensureTokenExists($requestData, $formIdentifier);
-
-        $sessionData = $_SESSION;
-
-        if (isset($sessionData[$this->tokenName][$formIdentifier]) && isset($requestData[$this->tokenName])) {
-            $tokenExpiration = $sessionData[$this->tokenName . '_expiration'][$formIdentifier];
-            if (time() < $tokenExpiration) {
-                return hash_equals($requestData[$this->tokenName], $sessionData[$this->tokenName][$formIdentifier]);
-            }
-        }
-
-        // Si el token es inválido o ha expirado, eliminamos el token antiguo y generamos uno nuevo
-        unset($_SESSION[$this->tokenName][$formIdentifier]);
-        unset($_SESSION[$this->tokenName . '_expiration'][$formIdentifier]);
-        $this->generateToken($formIdentifier);
-        return false;
-    }
-
-    private function _ensureTokenExists($requestData, $formIdentifier)
+    private function _tokenExists($requestData, $formIdentifier)
     {
         if (!isset($requestData[$this->tokenName])) {
-            unset($_SESSION[$this->tokenName][$formIdentifier]);
-            unset($_SESSION[$this->tokenName . '_expiration'][$formIdentifier]);
+            $this->_unsetTokenAndExpiration($formIdentifier);
             throw new ErrorHandler("CSRF token missing");
         }
     }
 
+    private function _isTokenInvalidOrExpired($formIdentifier)
+    {
+        return (!isset($_SESSION[$this->tokenName][$formIdentifier]) || 
+                time() >= $_SESSION[$this->tokenName . '_expiration'][$formIdentifier]);
+    }
+
+    private function _setTokenAndExpiration($formIdentifier)
+    {
+        $_SESSION[$this->tokenName][$formIdentifier] = $this->_generateRandomToken();
+        $_SESSION[$this->tokenName . '_expiration'][$formIdentifier] = time() + $this->tokenExpiration;
+    }
+
+    private function _unsetTokenAndExpiration($formIdentifier)
+    {
+        unset($_SESSION[$this->tokenName][$formIdentifier]);
+        unset($_SESSION[$this->tokenName . '_expiration'][$formIdentifier]);
+    }
+
+    private function isTokenValidAndNotExpired($requestData, $sessionData, $formIdentifier)
+    {
+        if (isset($sessionData[$this->tokenName][$formIdentifier]) && isset($requestData[$this->tokenName])) {
+            $tokenExpiration = $sessionData[$this->tokenName . '_expiration'][$formIdentifier];
+            if (time() < $tokenExpiration) {
+                return hash_equals($requestData[$this->tokenName], 
+                                   $sessionData[$this->tokenName][$formIdentifier]);
+            }
+        }
+
+        $this->_unsetTokenAndExpiration($formIdentifier);
+        $this->generateToken($formIdentifier);
+        return false;
+    }
+
     private function _generateRandomToken()
     {
-        return bin2hex(random_bytes($this->tokenLength));
+      return bin2hex(random_bytes($this->tokenLength));
     }
 }
