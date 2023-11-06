bcb5506435a509bc0ae82b03a4a0bd8d90025d19
pulsecodify <pulsecodi@gmail.com>
2023-11-05 22:50:18 -0500

    Reajuste

--- system/Router.php
+++ system/Router.php
@@ -1,110 +1,94 @@
+
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// Verificar si la constante BASEPATH está definida y si no, salir con un mensaje de error
-
-// Definición de la clase Router
 class Router
 {
-    private $routes; // Un arreglo para almacenar las rutas definidas en el archivo de configuración
+    private $routes;
 
-    // Constructor de la clase que toma el archivo de configuración como parámetro
     public function __construct($configFile)
     {
-        // Cargar las rutas desde el archivo de configuración
         $this->routes = include $configFile;
-
-        // Llamar al método _getUrl() para analizar y enrutar la URL de la solicitud
         $this->_getUrl();
     }
 
-    // Método privado para obtener la URL de la solicitud
     private function _getUrl()
     {
-        // Obtener la URL de la solicitud desde el parámetro 'url' en la consulta GET
-        if (!isset($_GET['url'])) {
-            $url = '/';
-        } else {
-            $url = $_GET['url'];
-        }
+        $url = isset($_GET['url']) ? $_GET['url'] : '/'; 
 
-        // Validar la URL
         if ($this->_isValidUrl($url)) {
             $url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
             return $this->_route($url);
         }
 
-        // En caso de URL no válida o no permitida, responder con un error 403 y un mensaje
         header('HTTP/1.1 403 Forbidden');
         echo 'Acceso no autorizado';
         exit;
     }
 
-    // Método privado para validar la URL (puedes personalizar la validación según tus necesidades)
     private function _isValidUrl($url)
     {
         return preg_match('/^[a-zA-Z0-9\/]+$/', $url);
     }
 
-    // Método privado para enrutar la solicitud
-    private function _route($url)
-    {
-        // Iterar a través de las rutas definidas en el archivo de configuración
-        foreach ($this->routes as $route => $target) {
-            $pattern = preg_replace('/:([a-zA-Z_]+)/', '([^/]+)', $route);
+  private function _route($url)
+  {
+      foreach ($this->routes as $route => $target) {
+          if ($this->_isMatchingRoute($route, $url, $matches, $pattern)) {
+              $this->_handleRouting($route, $matches, $target);
+              return;
+          }
+      }
+      $this->_handleNotFound($url);
+  }
 
-            // Comprobar si la URL coincide con el patrón de la ruta
-            if (preg_match("#^$pattern$#", $url, $matches)) {
-                list($controller, $method) = explode('@', $target);
-                $controllerPath = strtolower(str_replace("Controller", "", $controller));
+  private function _isMatchingRoute($route, $url, &$matches, &$pattern){
+      $pattern = preg_replace('/:([a-zA-Z_]+)/', '([^/]+)', $route);
+      return preg_match("#^$pattern$#", $url, $matches);
+  }
 
-                // Comprobar si existe el archivo del controlador
-                if (file_exists(CONTROLLERS_PATH . $controllerPath . '/' . $controller . '.php')) {
-                    require CONTROLLERS_PATH . $controllerPath . '/' . $controller . '.php';
-                } elseif (file_exists(CONTROLLERS_PATH . $controller . '.php')) {
-                    require CONTROLLERS_PATH . $controller . '.php';
-                } else {
-                    $this->_handleControllerNotFound($controller);
-                    return;
-                }
+  private function _handleRouting($route, $matches, $target){
+      list($controller, $method) = explode('@', $target);
+      if (!$this->_loadController($controller)) {
+          $this->_handleControllerNotFound($controller);
+      } elseif (class_exists($controller)) {
+          $this->_routeController($controller, $method, $matches, $route);
+      } else {
+          $this->_handleControllerNotFound($controller);
+      }
+  }
 
-                // Comprobar si la clase del controlador existe
-                if (class_exists($controller)) {
-                    $routeExpectsParams = (strpos($route, ':') !== false);
-                    $paramsProvided = count($matches) > 1;
+  private function _routeController($controller, $method, $matches, $route){
+      $routeExpectsParams = (strpos($route, ':') !== false);
+      $paramsProvided = count($matches) > 1;
+      if ($routeExpectsParams and !$paramsProvided) {
+          $this->_handleNoParameters();
+      } else {
+          $this->_callControllerMethod($controller, $method, array_slice($matches, $paramsProvided ? 1 : 0));
+      }
+  }
 
-                    // Manejar la llamada al método del controlador
-                    if ($routeExpectsParams) {
-                        if ($paramsProvided) {
-                            $this->_callControllerMethod($controller, $method, array_slice($matches, 1));
-                            return;
-                        } else {
-                            $this->_handleNoParameters();
-                            return;
-                        }
-                    } else {
-                        $this->_callControllerMethod($controller, $method);
-                        return;
-                    }
-                } else {
-                    $this->_handleControllerNotFound($controller);
-                    return;
-                }
-            }
+    // Load Controller function
+    private function _loadController($controller) {
+        $controllerPath = strtolower(str_replace("Controller", "", $controller));
+
+        if (file_exists(CONTROLLERS_PATH . $controllerPath . '/' . $controller . '.php')) {
+            require CONTROLLERS_PATH . $controllerPath . '/' . $controller . '.php';
+        } else if (file_exists(CONTROLLERS_PATH . $controller . '.php')) {
+            require CONTROLLERS_PATH . $controller . '.php';
+        } else {
+            return false;
         }
 
-        // Si no se encuentra ninguna ruta coincidente, manejar como ruta no encontrada
-        $this->_handleNotFound($url);
+        return true;
     }
 
-    // Método privado para llamar al método del controlador
     private function _callControllerMethod($controller, $method, $params = [])
     {
         $controllerInstance = new $controller();
         call_user_func_array([$controllerInstance, $method], $params);
     }
 
-    // Métodos privados para manejar diferentes situaciones de error
     private function _handleNoParameters() {
         throw new ErrorHandler("Error: El parámetro no ha sido proporcionado en la ruta.");
     }
