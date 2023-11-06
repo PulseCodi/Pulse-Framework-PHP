ac8d28213cfdec9d91bef4c5f7b9f41c5799037b
pulsecodify <pulsecodi@gmail.com>
2023-11-05 22:51:21 -0500

    Reajuste

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -4,5 +4,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $title ?></title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('/public/css/style.css') ?>">
 </head>
 <body>
